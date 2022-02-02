5cdb71627897443f63caaa8b30644b8ce3d662d9
Mahaaldajanii <maha050002@gmail.com>
2022-02-02 23:24:40 +0300

    Initial Commit

---
+++ Star1/Star1App.swift
@@ -0,0 +1,17 @@
+//
+//  Star1App.swift
+//  Star1
+//
+//  Created by Maha on 01/07/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct Star1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
